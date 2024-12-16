e4d79a347ffe3567999d4be69b0a06a05f19ff9c
Andrey <andrewnureev142@gmail.com>
2024-12-16 22:39:50 +0300

    Решение V 1.0 (убрать лишние потоки)

--- MPI_lab_2/MPI_lab_2.cpp
+++ MPI_lab_2/MPI_lab_2.cpp
@@ -1,20 +1,117 @@
-﻿// MPI_lab_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+#include <iomanip>
+#include <thread>
+#include <chrono>
+#include <cmath>
+#include "mpi.h"
+
+using namespace std;
+using namespace std::chrono;
+
+#define A 0
+#define B 1
+
+
+// Функция для численного интегрирования по формуле прямоугольников
+double rectangularIntegrate(double a, double b, int n) {
+    double len = (b - a) / n;
+    double result = 0.0;
+
+    for (int i = 0; i < n; ++i) {
+        double local_a = a + i * len;
+        double local_b = local_a + len;
+        result += (exp(local_a) + exp(local_b))/2;
+    }
+
+    result *= (b-a) / n;
+    return result;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    srand(static_cast<unsigned int>(time(NULL)));
+    
+    int Rank = 0;
+    int ProcessCount = 0;
+
+    MPI_Status Status;
+
+    int a = A;
+    int b = B;
+
+    double res;
+
+
+    MPI_Init(NULL, NULL);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcessCount);
+
+    int WorkerProcessCount = ProcessCount - 1;
+
+    steady_clock::time_point start = high_resolution_clock::now();
+
+
+    //Root процесс
+    if (Rank == 0) {
+
+        //Если процесс единственный
+        if (ProcessCount == 1) {
+
+            rectangularIntegrate(A, B, 10);
+
+            steady_clock::time_point end = high_resolution_clock::now();
+
+            cout << "Single-process run\n" << duration_cast<milliseconds>(end - start).count() << endl;
+
+            MPI_Finalize();
+
+            return 0;
+
+        }
+    }
+
+
+    //Отправка A
+    MPI_Bcast(&a, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //Отправка B
+    MPI_Bcast(&b, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //Отправка N
+    MPI_Bcast(&WorkerProcessCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    //cout << "Hello i am working proccess" << endl;
+
+    double len = ((b - a) / (double) WorkerProcessCount);
+    double local_a = a + Rank * len;
+    double local_b = local_a + len;
+    double local_res= (exp(local_a) + exp(local_b)) / 2;
+    local_res*= len;
+    if (Rank == WorkerProcessCount) { local_res = 0;} //TODO убрать процесс- корень (лишний поток)
+
+    
+    cout << a << "\t" << b << "\t" << WorkerProcessCount << "\t" << Rank << endl;
+    /*cout << "LOCAL len = " << len << endl;
+    cout << "LOCAL A = " <<  local_a << endl;
+    cout << "LOCAL B = " << local_b << endl;
+    */
+    cout << "LOCAL RES = " << local_res << endl;
+
+    MPI_Reduce(&local_res, &res, 1, MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    
+    if (Rank == 0) {
+        cout <<"FINAL RES = " << res << endl;
+        cout << rectangularIntegrate(A, B, 3) << endl;
+    }
+
+    MPI_Finalize();
+	
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
